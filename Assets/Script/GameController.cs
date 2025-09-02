003851daed268d3d26f90135db2d83b4cb34b401
Yunus Emre Usta <yunusemreusta.dev@gmail.com>
2025-09-02 21:28:38 +0300

    Implement countdown timer and end game screens with menu options

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,27 +1,61 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    public int TargerSuccess;
+    int InstantSuccess;
     int selectedNumber;
+    
     GameObject selectedButton;
-    GameObject theButtonItself;
+    GameObject currentButton;
+    
     public AudioSource[] voices;
     public GameObject[] buttons;
-
+    public TextMeshProUGUI counter;
+    public GameObject[] endGamePanels;
+    
+    float TotalTime = 10;
+    float Minute;
+    float Second;
+    bool Timer;
+    
     void Start()
     {
         selectedNumber = 0;
+        Timer = true;
+    }
+    
+    void Update()
+    {
+        if (Timer && TotalTime > 1)
+        {
+            TotalTime -= Time.deltaTime;
+            Minute = Mathf.FloorToInt(TotalTime / 60);
+            Second = Mathf.FloorToInt(TotalTime % 60);
+        
+            //counter.text = Mathf.FloorToInt(TotalTime).ToString();
+            counter.text = string.Format("{0:00}:{1:00}", Minute, Second);
+        }
+        else
+        {
+            counter.text = "Süre Bitti";
+            Timer = false;
+            GameOver();
+        }
+        
     }
 
     public Sprite defultSprite;
 
     public void giveObject(GameObject myObject)
     {
-        theButtonItself = myObject;
-        theButtonItself.GetComponent<Image>().sprite = theButtonItself.GetComponentInChildren<SpriteRenderer>().sprite;
-        theButtonItself.GetComponent<Image>().raycastTarget = false;
+        currentButton = myObject;
+        currentButton.GetComponent<Image>().sprite = currentButton.GetComponentInChildren<SpriteRenderer>().sprite;
+        currentButton.GetComponent<Image>().raycastTarget = false;
         voices[1].Play();
     }
 
@@ -46,7 +80,7 @@ public class GameController : MonoBehaviour
         if (selectedNumber == 0)
         {
             selectedNumber = IncomingValue;
-            selectedButton = theButtonItself;
+            selectedButton = currentButton;
         }
         else
         {
@@ -61,28 +95,49 @@ public class GameController : MonoBehaviour
 
         if (selectedNumber == IncomingValue)
         {
+            InstantSuccess++;
             selectedButton.GetComponent<Image>().enabled = false;
-            theButtonItself.GetComponent<Image>().enabled = false;
+            currentButton.GetComponent<Image>().enabled = false;
             selectedButton.GetComponent<Button>().enabled= false;
-            theButtonItself.GetComponent<Button>().enabled = false;
+            currentButton.GetComponent<Button>().enabled = false;
             selectedNumber = 0;
             selectedButton = null;
             SetButtonsActiveState(true);
+
+            if (TargerSuccess == InstantSuccess)
+            {
+                Win();
+            }
         }
         else
         {
             voices[2].Play();
             selectedButton.GetComponent<Image>().sprite = defultSprite;
-            theButtonItself.GetComponent<Image>().sprite = defultSprite;
+            currentButton.GetComponent<Image>().sprite = defultSprite;
             selectedNumber = 0;
             selectedButton = null;
             SetButtonsActiveState(true);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void GameOver()
     {
-        
+        endGamePanels[0].SetActive(true);
     }
+
+    void Win()
+    {
+        endGamePanels[1].SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
 }
